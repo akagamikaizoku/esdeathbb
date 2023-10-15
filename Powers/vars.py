215709b51a0d7f457f2071307a5028d5994d3c9b
Dhruv <142153463+akagamikaizoku@users.noreply.github.com>
2023-10-15 16:04:05 +0530

    Update vars.py

--- Powers/vars.py
+++ Powers/vars.py
@@ -11,42 +11,42 @@ class Config:
     """Config class for variables."""
 
     LOGGER = True
-    BOT_TOKEN = config("BOT_TOKEN", default=None)
-    API_ID = int(config("API_ID", default="123"))
-    API_HASH = config("API_HASH", default=None)
+    BOT_TOKEN = config("BOT_TOKEN", default="6663040181:AAEdRPMg1h8CaImyX5Wf8ERWNdUg8y97S-k")
+    API_ID = int(config("API_ID", default="29722120"))
+    API_HASH = config("API_HASH", default="91f3bf8651edfa748856cfcad07f55bb")
     OWNER_ID = int(config("OWNER_ID", default=1983471689))
-    MESSAGE_DUMP = int(config("MESSAGE_DUMP"))
+    MESSAGE_DUMP = int(config("MESSAGE_DUMP", default="-1001829052958"))
     DEV_USERS = [
         int(i)
         for i in config(
             "DEV_USERS",
-            default="",
+            default="1983471689",
         ).split(" ")
     ]
     SUDO_USERS = [
         int(i)
         for i in config(
             "SUDO_USERS",
-            default="",
+            default="1983471689",
         ).split(" ")
     ]
     WHITELIST_USERS = [
         int(i)
         for i in config(
             "WHITELIST_USERS",
-            default="",
+            default="1983471689",
         ).split(" ")
     ]
     GENIUS_API_TOKEN = config("GENIUS_API",default=None)
     AuDD_API = config("AuDD_API",default=None)
     RMBG_API = config("RMBG_API",default=None)
-    DB_URI = config("DB_URI", default="")
-    DB_NAME = config("DB_NAME", default="gojo_satarou")
+    DB_URI = config("DB_URI", default="mongodb+srv://helloyeager8:dSaini9485@cluster0.bhqp2fq.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp")
+    DB_NAME = config("DB_NAME", default="esdeath")
     BDB_URI = config("BDB_URI",default=None)
     NO_LOAD = config("NO_LOAD", default="").split()
-    PREFIX_HANDLER = config("PREFIX_HANDLER", default="/").split()
-    SUPPORT_GROUP = config("SUPPORT_GROUP", default="gojo_bots_network")
-    SUPPORT_CHANNEL = config("SUPPORT_CHANNEL", default="gojo_bots_network")
+    PREFIX_HANDLER = config("PREFIX_HANDLER", default="/ ! .").split()
+    SUPPORT_GROUP = config("SUPPORT_GROUP", default="chatdiscussion17")
+    SUPPORT_CHANNEL = config("SUPPORT_CHANNEL", default="esdeath_xb")
     WORKERS = int(config("WORKERS", default=16))
     TIME_ZONE = config("TIME_ZONE",default='Asia/Kolkata')
     BOT_USERNAME = ""
